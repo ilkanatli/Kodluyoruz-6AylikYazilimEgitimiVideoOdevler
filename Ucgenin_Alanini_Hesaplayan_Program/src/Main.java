3768def2a4bc9712435dce4916ab5f19d4f0ef3e
İlkan Atlı <atliilkan@gmail.com>
2023-05-24 22:55:28 +0300

    Üçgenin Alanını Hesaplayan Program Eklendi.

---
+++ Ucgenin_Alanini_Hesaplayan_Program/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        // Dik Üçgende Hipotenüs Bulan Program
+
+        // Değişkenleri tanımlayalım.
+        int a, b;
+        double c;
+
+        // Scanner sınıfını tanımladık.
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("1. Kenari giriniz: ");
+        a = scan.nextInt();
+
+        System.out.print("2. Kenari giriniz: ");
+        b = scan.nextInt();
+
+        c = Math.sqrt((a * a) + (b * b));
+        System.out.print("Hipotenus: " + c);
+         */
+        // Üçgenin Alanını Hesaplayan Program
+
+        // Değişkenleri tanımlayalım.
+        int a, b, c;
+        double u, alan;
+
+        // Scanner sınıfını tanımladık.
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("1. Kenari giriniz: ");
+        a = scan.nextInt();
+
+        System.out.print("2. Kenari giriniz: ");
+        b = scan.nextInt();
+
+        System.out.print("3. Kenari giriniz: ");
+        c = scan.nextInt();
+
+        // Üçgenin çevresi = 2u
+
+        u = (a + b + c) / 2;
+
+        double cevre = (2 * u);
+
+        System.out.println("Ucgenin cevresi: "  + cevre);
+
+        // Alan * Alan = 𝑢 * (𝑢 − 𝑎) * (𝑢 − 𝑏) * (𝑢 − 𝑐)
+
+        alan = Math.sqrt(u * (u - a) * (u - b) * (u- c));
+
+        System.out.println("Ucgenin alani: " + alan);
+    }
+}
