07f6295df8a45bd108b93dd4d2f6bcc7bcb6a11d
İlkan Atlı <atliilkan@gmail.com>
2023-08-25 23:58:44 +0300

    Turizm Acente Sistemi

---
+++ TourismAgencySystem/src/Main.java
@@ -0,0 +1,17 @@
+import com.tourismAgencySystem.model.User;
+import com.tourismAgencySystem.view.LoginGUI;
+
+public class Main {
+    private User user;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Tourism Agency System");
+        //SearchRoomGUI searchRoomGUI = new SearchRoomGUI(new User());
+        LoginGUI loginGUI = new LoginGUI();
+        //HotelAddGUI hotelAddGUI = new HotelAddGUI();
+        //RoomGUI roomGUI = new RoomGUI();
+        //RoomAddGUI roomAddGUI = new RoomAddGUI();
+        //SearchOutputGUI searchOutputGUI = new SearchOutputGUI();
+        //RezCompleteGUI rezCompleteGUI = new RezCompleteGUI();
+    }
+}
